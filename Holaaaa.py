28be64b6f40733531efe6ddc530e9d7d7fa17d22
Baldorpau07 <Baldorpau07@ciencias.unam.mx>
2025-10-02 00:43:25 -0600

    Add files via upload

---
+++ Holaaaa.py
@@ -0,0 +1,31 @@
+print(" de flotante %f a entero %d \n"%(2.579,int(2.579)))
+print(" de cadena %s a flotante %f  \n"%("5",float("5")))
+print(" de cadena %s a flotante %f  \n"%("6",float("6.666")))
+print("%.10f" % 2.0**10)
+print("%.5e" %0.0000006456)
+print("{:+.2f}".format(+3.657578))
+nombre="Paula"
+edad=18
+print(f"Hola, me llamo {nombre} y tengo {edad} años")
+edad=float(input("¿cuántos años tienes"))
+años_te_faltan=100-edad
+print(f"Te faltan {años_te_faltan} para cumplir 100 años")
+if 5>2:
+    print("5 es mayor que 2")
+else:
+  print("5 es menor que 2")
+#holaaaa
+"""hjjbhbhqsbahvhasjhsaabmahaa
+"""
+15//5
+26%5
+3**5
+entero=5
+print('el tipo de dato de %d es %s'%(entero,type(5)))
+flotante=5.0
+print('el tipo de dato de %f es  %s'%(flotante,type(5.0)))
+cadena="5"
+print('el tipo de dato de %s es %s'%(cadena,type("5")))
+boleano=True
+print("el tipo de dato de %r es %s"%(boleano,type(True)))
+
